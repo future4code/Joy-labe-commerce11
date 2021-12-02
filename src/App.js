d6286cf6b3ac528c3cb6cecdc7bb755deb9cf1d5
Rodrigo Rodrigues <rodriigo.rc@gmail.com>
2021-12-01 23:27:42 -0300

    Base do projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Filtros from './components/Filtros';
+import Carrinho from './components/Carrinho';
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MainContainer = styled.div`
+  display: flex;
+  
+
+`
+const ContainerProdutos = styled.div`
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+
+`
+
+export default class App extends React.Component{
+
+  render(){
+
+    return(
+      <MainContainer>
+        <Filtros>
+          
+        </Filtros>
+        
+
+        <ContainerProdutos>
+          
+          
+          <h1>Produtos</h1>
+        </ContainerProdutos>
+
+        <Carrinho></Carrinho>
+      </MainContainer>
+    )
+  }
+
+
 }
 
-export default App;
