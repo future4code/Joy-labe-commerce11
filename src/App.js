98df0ad2932035b3f231ca2a8770128970021a95
Rodrigo Rodrigues <rodriigo.rc@gmail.com>
2021-12-03 00:08:47 -0300

    Implementado card com produtos

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import Filtros from './components/Filtros';
 import Carrinho from './components/Carrinho';
 import styled from "styled-components";
+import Produto from "./components/Produto";
 
 const MainContainer = styled.div`
-  display: flex;
-  
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 
 `
 const ContainerProdutos = styled.div`
-  display: flex;
-  flex-grow: 1;
-  justify-content: center;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
 
+`
+const ContainerHome = styled.div`
+  text-align: center;
 `
 
 export default class App extends React.Component{
@@ -21,18 +25,18 @@ export default class App extends React.Component{
 
     return(
       <MainContainer>
-        <Filtros>
-          
-        </Filtros>
-        
+        <Filtros />
+        <ContainerHome>
+          <h1>Produtos</h1>  
+          <ContainerProdutos>
+            <Produto foto="https://picsum.photos/200/321" produto="produto 1" preco="Preço: R$ 100" />
+            <Produto foto="https://picsum.photos/200/320" produto="Produto 2" preco="Preço: R$ 200" />
+            <Produto foto="https://picsum.photos/200/310" produto="Produto 3" preco="Preço: R$ 300" />
+            <Produto foto="https://picsum.photos/200/370" produto="Produto 4" preco="Preço: R$ 400" />
+          </ContainerProdutos>
+        </ContainerHome>
 
-        <ContainerProdutos>
-          
-          
-          <h1>Produtos</h1>
-        </ContainerProdutos>
-
-        <Carrinho></Carrinho>
+        <Carrinho />
       </MainContainer>
     )
   }
