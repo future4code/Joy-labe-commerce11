3adfe1df43247a651bdafdb50462526c053fb99e
Rodrigo Rodrigues <rodriigo.rc@gmail.com>
2021-12-03 22:36:58 -0300

    Modificações no carrinho

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,13 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import ItemCarrinho from "./ItemCarrinho";
 
 const ContainerCarrinho = styled.div`
     display: flex;
+    flex-direction: column;
+    padding-left: 6px;
     margin-right: 10px;
-    width: 20vw;
+    width: 19.4vw;
     height: 100%;
     border: 1px solid black;
-    justify-content: center;
+    justify-content: start;
+    align-items: flex-start; 
 
 `
 
@@ -18,6 +22,11 @@ export default class Carrinho extends React.Component{
         return(
             <ContainerCarrinho>
                 <h3>Carrinho</h3>
+                <ItemCarrinho quantidade="1x" produto="Caneca astronauta" />
+                <ItemCarrinho quantidade="1x" produto="Camiseta astronauta" />
+                <ItemCarrinho quantidade="1x" produto="Foguete espacial" />
+                <ItemCarrinho quantidade="1x" produto="Traje astronauta" />
+                <p>Valor total: R$ 100,00</p>
             </ContainerCarrinho>
         )
     }
